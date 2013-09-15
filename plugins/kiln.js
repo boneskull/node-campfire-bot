4617764fb3e58c0300e55a4c0fc249f41e4be6a5
Christopher Hiller <chiller@badwing.com>
2013-09-15 12:29:43 -0700

    kiln reporting at interval (default 15m)

--- plugins/kiln.js
+++ plugins/kiln.js
@@ -1,6 +1,24 @@
 var kiln = require('kiln'),
   format = require('util').format,
-  speak = require('../lib/actions').speak;
+  speak = require('../lib/actions').speak,
+  conf = require('../kiln-plugin.conf.json');
+
+var queue = {}, t,
+  report = function report() {
+    var roomIds = Object.keys(queue),
+      i = roomIds.length, roomId, bodies;
+    while (i--) {
+      roomId = roomIds[i];
+      if (queue[roomId].length) {
+        bodies = queue[roomId].join('\n');
+        speak({
+          type: 'PasteMessage',
+          body: bodies
+        }, roomId);
+        queue[roomId] = [];
+      }
+    }
+  };
 
 module.exports = [
   {
@@ -10,28 +28,25 @@ module.exports = [
         while (i--) {
           speak({
             type: 'TextMessage',
-            body: format('Connected to feed %s', meta['rss:title']['#'])
+            body: format('Connected to feed [%s]', meta['rss:title']['#'])
           }, rooms[i].id);
+          queue[rooms[i].id] = [];
+          t = setInterval(report, conf.reportInterval)
         }
       }
 
       function onData(data) {
-        var i = rooms.length;
+        var i = rooms.length, roomId;
         while (i--) {
+          roomId = rooms[i].id;
           if (data['rss:title']['#'].indexOf('Automated merge') === -1 &&
               data['rss:title']['#'].indexOf('resolved review') === -1) {
-            speak({
-              type: 'TextMessage',
-              body: format('%s pushed changeset %s: %s',
-                data['rss:author']['#'],
-                data['rss:link']['#'], data['rss:title']['#'])
-            }, rooms[i].id);
+            queue[roomId].push(format('%s pushed changeset %s: %s',
+              data['rss:author']['#'],
+              data['rss:link']['#'], data['rss:title']['#']));
           } else if (data['rss:title']['#'].indexOf('resolved review') >= 0) {
-            speak({
-              type: 'TextMessage',
-              body: format('%s: %s',
-                data['rss:link']['#'], data['rss:title']['#'])
-            }, rooms[i].id);
+            queue[roomId].push(format('%s: %s',
+              data['rss:link']['#'], data['rss:title']['#']));
           }
         }
       }
